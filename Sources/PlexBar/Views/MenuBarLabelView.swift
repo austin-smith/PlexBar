bc7d5802fc05a7865f8e1caa38ebbab73bb19c3d
Austin Smith <austinsmith23@gmail.com>
2026-04-11 23:12:15 -0700

    update menu bar icon

--- Sources/PlexBar/Views/MenuBarLabelView.swift
+++ Sources/PlexBar/Views/MenuBarLabelView.swift
@@ -5,7 +5,7 @@ struct MenuBarLabelView: View {
 
     var body: some View {
         HStack(spacing: 6) {
-            Image(systemName: streamCount > 0 ? "play.rectangle.fill" : "play.rectangle")
+            Image(systemName: "popcorn.fill")
 
             if streamCount > 0 {
                 Text("\(streamCount)")
