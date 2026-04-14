ac4ff7d1bcb41122e34078a5e5f7e356ff12323e
Austin Smith <austinsmith23@gmail.com>
2026-04-14 02:04:53 -0700

    bump version number

--- Sources/PlexBar/Support/AppConstants.swift
+++ Sources/PlexBar/Support/AppConstants.swift
@@ -3,7 +3,7 @@ import Foundation
 enum AppConstants {
     static let appName = "PlexBar"
     static let bundleIdentifier = "com.crapshack.PlexBar"
-    static let productVersion = "0.1.0"
+    static let productVersion = "0.2.0"
     static let settingsWindowID = "settings"
     static let defaultPollIntervalSeconds = 15
     static let minimumPollIntervalSeconds = 5
